c6f2440753242b2f129bae11729d902bff1e816d
sdehlawi <sdehlawi@gmail.com>
2018-03-19 19:56:51 -0700

    Add files via upload

--- uArmMovement.ino
+++ uArmMovement.ino
@@ -1,92 +1,148 @@
 
+/*Code for Uarm Arduino Uno movement and receiving input from Mega
+ * 
+ * 
+ */
+
 #include "uArm.h"
 #include "uArmAPI.h"
-#define USE_SERIAL_CMD  1 // 1: use serial for control  0: just use arduino to control(release ROM and RAM space)
 
 unsigned long tickStartTime = millis(); // get timestamp;
 static void Init();
+
 int box1[] = {-151.5, 130, 155};
 int box2[] = {-154, 234, 156};
-int input = 0;
-//int storage[] = {};
+int diceBox[] = {-100 , 182,155}; // directly left of the storage area
+int sliceBox[] = {-50, 182,155};  // slightly more left than above coordinates
+//int input = 0;
+
+const byte numChars = 32;
+char receivedChars[numChars];
+char tempChars[numChars];        // temporary array for use when parsing
+
+      // variables to hold the parsed data
+char cutFromMega[numChars] = {0};
+int boxFromMega = 0;
+//float floatFromPC = 0.0;
+
+boolean newData = false;
 
 void setup()
 {
+
   Serial.begin(9600);
-  uArmInit(); // Don't remove
+  Init(); // Don't remove
   //uarm.init();
-  debugPrint("debug start"); // uncomment DEBUG in uArmConfig.h to use debug function
+  //debugPrint("debug start"); // uncomment DEBUG in uArmConfig.h to use debug function
   //pinMode(2,INPUT); //TipSensor
   // TODO
-  moveTo(0, 150, 150); //Neutral Position coordinates
+  moveTo(0, 150, 150, 50); //Neutral Position coordinates
   Serial.println("@1"); // report ready
-  reportPos(); //Serial Prints the current position
-//  moveTo(box1[0],box1[1],box1[2]);  //Move to Box 1
-//  Serial.println("@1"); // report ready
-//  reportPos(); //Serial Prints the current position
-//  int boxZ = box1[2];
-//  while (getTip()) {
-//    boxZ = boxZ - 1;
-//    //box1[1] = box1[1] + 5;
-//    //delay(1000);
-//    moveTo(box1[0],box1[1],boxZ, 50);  //Move to Box 1
-//    Serial.println("@1"); // report ready
-//    reportPos(); //Serial Prints the current position
-//    //delay(150);
-//  }
-//  delay(1000);
-//  pumpOn();
-//  reportPos();
-//  delay(1000);
-//  moveTo(box1[0],box1[1], 200, 100);
-//  delay(3000);
-//  moveTo(207.5, 128.6, 150, 100);
-//  delay(3000);
-//  pumpOff();
-//  delay(3000);
-//  moveTo(0, 150, 150); //Neutral Position coordinates
-//  Serial.println("@1"); // report ready
-//  reportPos(); //Serial Prints the current position
-  
-
-}
-
-void loop()
-{
-  run(); // Don't remove
+  //reportPos(); //Serial Prints the current position
   Serial.println("Please input 1 or 2");
-  while (Serial.available() == 0) {} //do nothing until input
-  if (Serial.available() > 0){
-    input = Serial.read();
-    //input.toCharArray
-    Serial.println(input - '0');
-    if (input == '1'){
-      getFood(box1);
-      //input = '0';
-    }else if (input == '2'){
-      getFood(box2);
-      //input = '0';
-    }/*else {
-      Serial.println("Please input 1 or 2");
-    }*/
-    //Serial.print("Input : "); 
-    //Serial.println(atoi(input));
-    
-  }
-  // TODO
-  
-  /*moveTo(box1[0],box1[1],box1[2]);  //Move to Box 1
-  Serial.println("@1"); // report ready
-  reportPos(); //Serial Prints the current position
-  delay(1000);
-  moveTo(box2[0],box2[1],box2[2]);  //Move to Box 2
-  Serial.println("@1"); // report ready
-  reportPos(); //Serial Prints the current position
-  delay(1000);
-  */
 }
 
-void getFood(int storage[])
+void loop(){
+  int processLoc[] = {0,0,0};
+  run(); // Don't remove
+  recvWithStartEndMarkers();
+ 
+   
+  if (newData == true) {
+    strcpy(tempChars, receivedChars);
+    // this temporary copy is necessary to protect the original data
+    // because strtok() used in parseData() replaces the commas with \0
+    parseData();
+    showParsedData();
+    
+    //Serial.print("Choose box ... ");
+    //Serial.println(receivedChars);
+    //Serial.println(receivedChars[1] - '0');
+    if(strcmp(cutFromMega,"d")== 0){
+      processLoc = diceBox;
+    } else if (strcmp(cutFromMega,"s")==0){
+      processLoc = sliceBox;
+    } else {
+      Serial.print("Inoperable process selected you chose: ");
+      Serial.println(cutFromMega);
+    }
+    if (boxFromMega == 1){
+      Serial.print("Get food from box 1, cut ");
+      Serial.println(cutFromMega);
+      getFood(box1, processLoc);
+    }else if (boxFromMega == 2){
+      Serial.print("Get food from box 2, cut ");
+      Serial.println(cutFromMega);
+      getFood(box2, processLoc);
+    } else {
+      Serial.println("You didn't pick a food box, jackass.");
+    }
+    Serial.write(cutFromMega);
+    //Serial.println("Please input 1 or 2");
+    newData = false;
+  }
+  
+  //Serial.print("Input : "); 
+  //Serial.println(atoi(input));    
+}
+
+
+void recvWithStartEndMarkers() {
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  char startMarker = '<';
+  char endMarker = '>';
+  char rc;
+
+  while (Serial.available() == 0) {} //do nothing until input
+  while ((Serial.available() > 0) && newData == false){
+    rc = Serial.read();
+    if (recvInProgress == true) {
+      if (rc != endMarker) {
+        receivedChars[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars) {
+          ndx = numChars - 1;
+        }
+      } else {
+        receivedChars[ndx] = '\0'; // terminate the string
+        recvInProgress = false;
+        ndx = 0;
+        newData = true;
+      }
+    } else if (rc == startMarker) {
+      recvInProgress = true;
+    }
+  }
+}
+
+void parseData() {      // split the data into its parts
+
+    char * strtokIndx; // this is used by strtok() as an index
+
+    strtokIndx = strtok(tempChars,",");      // get the first part - the string
+    strcpy(cutFromMega, strtokIndx); // copy it to messageFromPC
+ 
+    strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
+    boxFromMega = atoi(strtokIndx);     // convert this part to an integer
+
+    //strtokIndx = strtok(NULL, ",");
+    //floatFromPC = atof(strtokIndx);     // convert this part to a float
+
+}
+
+//============
+
+void showParsedData() {
+    Serial.print("Type of cut: ");
+    Serial.println(cutFromMega);
+    Serial.print("Food from box: ");
+    Serial.println(boxFromMega);
+    //Serial.print("Float ");
+    //Serial.println(floatFromPC);
+}
+
+void getFood(int storage[], int processing[])
 {
   //delay(1000);
   int storageZ = storage[2];
@@ -107,7 +163,7 @@ void getFood(int storage[])
   delay(1000);
   moveTo(storage[0],storage[1], 200, 100);
   delay(1000);
-  moveTo(207.5, 128.6, 200, 100); //To the cutting Module
+  moveTo(processing[0], processing[1], processing[2], 100); //To the cutting Modules
   delay(1000);
   pumpOff();
   delay(1000);
